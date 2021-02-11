686d26cfa7a406f666ceef78724a765d1e6ffa79
admin <admin@DESKTOP-CRCOQ1N>
2021-02-11 15:02:06 +0530

    First Commit.....

---
+++ src/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int a=5;
+	int b=4;
+	int sum = a+b;
+	System.out.println(sum);
+		
+	}
+
+}
